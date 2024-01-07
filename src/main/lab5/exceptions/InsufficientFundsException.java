9da040e9a0bba19fcfd7feb53a7dfff331f60644
VladaLenda <91739352+VladaLenda@users.noreply.github.com>
2024-01-07 04:09:16 +0200

    Update InsufficientFundsException.java

--- src/main/lab5/exceptions/InsufficientFundsException.java
+++ src/main/lab5/exceptions/InsufficientFundsException.java
@@ -1,11 +1,5 @@
-package com.bondarenko.universityAssigment.lab5.exceptions;
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String errorMessage) {
-        super(errorMessage);
-    }
-
-    public InsufficientFundsException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
     }
 }
