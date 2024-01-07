09db3a0dfce7115acee0498b2d7ee71bb1b593b9
VladaLenda <91739352+VladaLenda@users.noreply.github.com>
2024-01-07 04:09:39 +0200

    Update NegativeAmountException.java

--- src/main/lab5/exceptions/NegativeAmountException.java
+++ src/main/lab5/exceptions/NegativeAmountException.java
@@ -1,11 +1,5 @@
-package com.bondarenko.universityAssigment.lab5.exceptions;
-
-public class NegativeAmountException extends RuntimeException {
-    public NegativeAmountException(String errorMessage) {
-        super(errorMessage);
-    }
-
-    public NegativeAmountException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
+public class NegativeAmountException extends Exception {
+    public NegativeAmountException(String message) {
+        super(message);
     }
 }
